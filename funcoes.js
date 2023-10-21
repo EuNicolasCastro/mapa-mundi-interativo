960c96326c2d1b3c092fda939f6a6570f681cf9a
EuNicolasCastro <98694704+EuNicolasCastro@users.noreply.github.com>
2023-10-21 00:20:10 -0300

    atualzando

--- funcoes.js
+++ funcoes.js
@@ -1,12 +1,12 @@
-async function getData(place){
-    
-    const url = `https://timezone.abstractapi.com/v1/current_time/?api_key=6ea5b4b8836b45d8bb1dc173ed180a55&location=b${place}` 
-    const res= await fetch(url)
-    
-    data = await res.json()
-    time = data.datetime
-    document.getElementById("time").style.opacity = 1
-    document.getElementById("time").innerText = `This is ${place}`
+    async function getData(place){
+        
+        const url = `https://timezone.abstractapi.com/v1/current_time/?api_key=6ea5b4b8836b45d8bb1dc173ed180a55&location=b${place}` 
+        const res= await fetch(url)
+        
+        data = await res.json()
+        time = data.datetime
+        document.getElementById("time").style.opacity = 1
+        document.getElementById("time").innerText = `This is ${place}`
     }
 
     document.querySelectorAll('.allPaths').forEach(e=>{
@@ -14,10 +14,9 @@ async function getData(place){
             window.onmousemove=function(j){
                 x=j.clientX
                 y=j.clientY
-                document.getElementById("name").style.top =  y-30+"px"
+                document.getElementById("name").style.top = y-30+"px"
                 document.getElementById("name").style.left = x+10+"px"
             }
-
             e.style.fill="pink"
             document.getElementById("name").style.opacity=1
             document.getElementById("namep").innerText=e.id
